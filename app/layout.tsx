9da97d3b02e451691a1fe3c982a7368491b7279f
Dheeraj-Manwani <dkmanwani2000@gmail.com>
2026-03-17 21:49:43 +0530

    case issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Providers } from "@/components/Providers";
+import { Providers } from "@/components/providers";
 import fullLogo from "@/assets/full_logo.png";
 import appLogo from "@/assets/logo.png";
 
